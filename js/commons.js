fa98c0484fe614a3d5faf36b18cda8c0d2a0cfdf
Bright Obiri Gyan <74192474+Bbrightr@users.noreply.github.com>
2025-03-18 23:50:49 +0100

    header Js. Updated

--- js/commons.js
+++ js/commons.js
@@ -1,7 +1,141 @@
 /*-----------------------------------------------------------------------------------*/
 /*  Page Header
 /*-----------------------------------------------------------------------------------*/
+const headerNav = document.getElementById('header')
+headerNav.innerHTML = `
+            <header id="header" class="clearfix">
+                <div class="site-header">
+                    <div class="container">
+                        <div class="row">
+                            <div class="top-header clearfix">
+                                <div class="logo col-md-4">
+                                    <a href="index.html">
+                                        <img src="img/lg_new.jpg" alt="Galore Logistics" height="70" width="235" />
+                                    </a>
+                                </div>
 
+                                <div class="info col-md-8">
+                                    <ul>
+                                        <li>
+                                            <i class="fa fa-clock-o"></i>
+                                            <p>
+                                                <span class="heading">Opening Hours</span>
+                                                <span>Mon - Fri: 8am - 5pm</span>
+                                            </p>
+                                        </li>
+                                        <li>
+                                            <i class="fa fa-phone"></i>
+                                            <p>
+                                                <span class="heading">Call Us</span>
+                                                <span>+233 (0)57 770 7080</span>
+                                            </p>
+                                        </li>
+                                        <li class="last">
+                                            <i class="fa fa-envelope"></i>
+                                            <p>
+                                                <span class="heading">email</span>
+                                                <span>info@galorelogistics.com</span>
+                                            </p>
+                                        </li>
+                                    </ul>
+                                </div>
+
+                                <div class="slide-buttons">
+                                    <button id="slide-buttons" class="slide-button icon icon-navicon"></button>
+                                </div>
+                            </div>
+
+                            <div class="navigation">
+                                <div class="main-menu">
+                                    <ul class="menu">
+                                        <li><a href="index.html">Home</a></li>
+                                        <li><a href="about.html">About Us</a></li>
+                                        <li class="has-sub"><a href="services.html">Services</a>
+                                            <ul>
+                                               <li><a href="cbrokerage.html">Customs Brokerage</a></li>
+                                               <li><a href="exportdoc.html">Export Documentation</a></li>
+                                               <li><a href="fclcl.html">Confirming House </a></li>
+                                               <li><a href="projectcargo.html">Project Cargo</a></li>
+                                               <li><a href="prefinance.html">Pre - finance of export</a></li>
+											   <li><a href="fnegociation.html">Freight negotiation</a></li>
+                                               <li class="last"><a href="advice.html">Cargo insurance advice</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="tracking.html">Tracking</a></li>
+                                        <li><a href="contact.html">Contact</a></li>
+                  </ul>
+
+                                    <div class="right-section">
+                                        <div class="search">
+                                            <i class="fa fa-search"></i>
+                                        </div>
+                                        <div class="quote-link">
+                                            <a href="contact.html">Request a Quote</a>
+                                        </div>
+                                        <div class="search-input">
+                                            <input type="text" name="s" placeholder="Type some text in here..." class="untouched">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Menu Mobile
+                ============================================= -->
+                <nav id="c-menu--slide-right" class="c-menu c-menu--slide-right">
+                    <button class="c-menu__close icon icon-close"></button>
+
+                    <div class="search-input">
+                        <input type="text" name="s" placeholder="Type some text in here..." class="untouched">
+                        <button><i class="fa fa-search"></i></button>
+                    </div>
+
+                    <ul class="slide-menu-items">
+                        		<li><a href="index.html">Home</a></li>
+                                        <li><a href="about.html">About Us</a></li>
+                                        <li class="has-sub"><a href="services.html">Services</a>
+                                            <ul>
+                                               <li><a href="cbrokerage.html">Customs Brokerage</a></li>
+                                               <li><a href="exportdoc.html">Export Documentation</a></li>
+                                               <li><a href="fclcl.html">Confirming House </a></li>
+                                               <li><a href="projectcargo.html">Project Cargo</a></li>
+                                               <li><a href="prefinance.html">Pre - finance of export</a></li>
+											   <li><a href="fnegociation.html">Freight negotiation</a></li>
+                                               <li class="last"><a href="advice.html">Cargo insurance advice</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="tracking.html">Tracking</a></li>
+                                        <li><a href="contact.html">Contact</a></li>
+                  </ul>
+
+                    <div class="info">
+                        <ul>
+                            <li>
+                                <p>
+                                    <span class="heading">Opening Hours</span>
+                                    <span>Mon - Sat: 8am - 5pm</span>                                </p>
+                            </li>
+                            <li>
+                                <p>
+                                    <span class="heading">Call Us</span>
+                                    <span>+233 (0)303 9330 70</span>                                </p>
+                            </li>
+                            <li class="last">
+                                <p>
+                                    <span class="heading">email</span>
+                                    <span>info@galorelogistics.com</span>                                </p>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+
+                <div id="slide-overlay" class="slide-overlay"></div>
+                <!-- Menu Mobile End -->
+            </header>
+
+`
 
 /*-----------------------------------------------------------------------------------*/
 /*  Page Footer
